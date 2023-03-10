f642fd3aa12ac348ce6da2e10675e26f056f06a4
MohsenSami <mohsensami88@gmail.com>
2023-03-10 21:32:17 +0330

    install SW

---
+++ public/serviceworker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+
+const self = this;
+
+// Install SW
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Opened cache");
+
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Listen for requests
+
+// Activate the SW
